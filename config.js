8631df72e72a5bdbf16aaa7f987a7f516e56653f
zfrond <zfrond@zfrond.com>
2024-05-16 20:26:54 +0200

    refactoring

---
+++ config.js
@@ -0,0 +1,10 @@
+const config = {
+    tokenString  : "6471422980:AAHQP-13dn8OC_P2KNTEKsHoLc0IrLhSXcI",
+    botName: "your_bot",
+    serverAddress: "",
+    serverPort: "",
+    storage: {
+      db: "./db"
+    }
+}
+module.exports = config
--- index.js
+++ index.js
@@ -1,12 +1,5 @@
-const config = {
-    tokenString  : "6471422980:AAHQP-13dn8OC_P2KNTEKsHoLc0IrLhSXcI",
-    botName: "your_bot",
-    serverAddress: "",
-    serverPort: "",
-    storage: {
-      db: "./db"
-    }
-}
+
+const config = require('./config.js')
 
 //deps
 const TelegramBot = require('node-telegram-bot-api')
